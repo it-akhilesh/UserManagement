ddee6f94547782d9c1d9a31a77a755e3d2fd96c5
Akhilesh Singh <360akhileshsingh.it@gmail.com>
2026-03-20 02:26:46 +0530

    Updated connection string

--- UserManagement/UserManagement/Global.asax
+++ UserManagement/UserManagement/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserManagement.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserManagement.WebApiApplication" Language="C#" %>
--- UserManagement/UserManagement/Global.asax.cs
+++ UserManagement/UserManagement/Global.asax.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
+using System.Web.Http;
 
 namespace UserManagement
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            SwaggerConfig.Register(GlobalConfiguration.Configuration);
         }
     }
 }
