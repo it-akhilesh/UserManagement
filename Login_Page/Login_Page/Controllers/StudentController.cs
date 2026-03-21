c1222180891cf59e67bfcf45d9ce7e2f2479db49
Akhilesh Singh <360akhileshsingh.it@gmail.com>
2026-03-21 11:54:05 +0530

    Added student form

--- Login_Page/Login_Page/Controllers/StudentController.cs
+++ Login_Page/Login_Page/Controllers/StudentController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Login_Page.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,9 +13,30 @@ namespace Login_Page.Controllers
         // GET: Student
         public ActionResult Index()
         {
+            StudentViewModel stu = new StudentViewModel();
+
             ViewBag.Username = Session["username"];
             ViewData["Role"] = Session["role"];
-            return View();
+
+            stu.CountryList = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "India", Text = "India" },
+                    new SelectListItem { Value = "USA", Text = "United States" },
+                };
+            stu.StateList = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "Uttar Pradesh", Text = "Uttar Pradesh" },
+                    new SelectListItem { Value = "Uttrakhand", Text = "Uttrakhand" },
+                };
+            stu.CityList = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "lko", Text = "Lucknow" },
+                    new SelectListItem { Value = "Mana", Text = "Manali" },
+                };
+
+            return View(stu);
+
+
         }
     }
 }
