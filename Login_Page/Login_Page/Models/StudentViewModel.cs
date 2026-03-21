cb9f6489653c747f6c19976d8483d10958b433d3
Akhilesh Singh <360akhileshsingh.it@gmail.com>
2026-03-21 11:57:27 +0530

    Added missing model

---
+++ Login_Page/Login_Page/Models/StudentViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_Page.Models
+{
+    public class StudentViewModel
+    {        
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public List<SelectListItem> CountryList { get; set; }
+        public string State { get; set; }
+        public List<SelectListItem> StateList { get; set; }
+        public string City { get; set; }
+        public List<SelectListItem> CityList { get; set; }
+
+        public string PinCode { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+
+        [Display(Name = "Upload Photo")]
+        public HttpPostedFileBase Photo { get; set; }
+    }
+}
